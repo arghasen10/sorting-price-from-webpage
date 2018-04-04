74dbfb9990c0a2240b84c3d65796e5cb252f0df2
ARGHA SEN <arghasen10@gmail.com>
2018-04-05 03:38:59 +0530

    Add files via upload

---
+++ flipkart_sort_by_price.py
@@ -0,0 +1,26 @@
+import urllib.request
+import bs4 as bs
+s=input('what are you searching?')
+s=s.replace(" ","%20")
+url='https://www.flipkart.com/search?q='+s+'&otracker=start&as-show=on&as=off'
+sauce=urllib.request.urlopen(url)
+source_code=sauce.read()
+soup=bs.BeautifulSoup(source_code,'lxml')
+price_list=[]
+for tshirt in soup.find_all('div',{'class':'_3liAhj'}):
+    title=tshirt.find('a',{'class':"_2cLu-l"})
+    #print(title.text)
+    price = tshirt.find('div',{'class':'_1vC4OE'})
+    item = price.text
+    l = list(item)
+    del (l[0])
+    item = "".join(l)
+    price_list.append(item)
+l=[]
+for i in price_list:
+    try:
+        it=int(i)
+        l.append(it)
+    except:
+        pass
+print(min(l))
